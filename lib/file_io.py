c25f5c536797cde23b4a2a8b8e839853dba606ab
LabanMugutu <labanmugutu@gmail.com>
2025-10-21 17:47:03 +0300

    Finishes assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,27 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    """
+    Creates or overwrites a .txt file with the given content.
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends new content to an existing .txt file.
+    If the file doesn't exist, it creates one.
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Reads and returns the content of a .txt file.
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
+
+
